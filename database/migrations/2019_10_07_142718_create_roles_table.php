5904b1fb8e685673a4e82dafc3d384af50a94f9f
elnurbugirov <info.elnur@yahoo.com>
2019-10-07 21:52:58 +0400

    admin master page setup

--- database/migrations/2019_10_07_142718_create_roles_table.php
+++ database/migrations/2019_10_07_142718_create_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('role_id')->index()->unsigned()->nullable();
-            $table->integer('is_active')->default(0);
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,18 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+    .styles([
+        'resources/css/libs/blog-post.css',
+        'resources/css/libs/bootstrap.css',
+        'resources/css/libs/font-awesome.css',
+        'resources/css/libs/metisMenu.css',
+        'resources/css/libs/sb-admin-2.css'
+    ],'./public/css/libs.css')
+    .scripts([
+        'resources/js/libs/jquery.js',
+        'resources/js/libs/bootstrap.js',
+        'resources/js/libs/metisMenu.js',
+        'resources/js/libs/sb-admin-2.js',
+        'resources/js/libs/scripts.js'
+    ],'./public/js/libs.js');
